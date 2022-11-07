68f20419d76b1ef9b288180dc0ecbcf5d71cb8ec
Eric Harris <ericoldie@gmail.com>
2022-11-07 07:42:07 -0500

    add updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
 "use strict";
 
-window.onload = function () {
-  getRoomRate();
-};
+const totalRoomCost = document.getElementById('totalRoomCost');
+const totalDiscountCost = document.getElementById("totalDiscountCost");
+const totalAdultChildren = document.getElementById("totalAdultChildren");
+const totalCost = document.getElementById("totalCost");
 
-function getRoomRate() {
+
+
+function getRoomRate(event) {
+    event.preventDefault(); 
   // price depends on in-season and out-of-season rates based on the checkin-date.
   const checkInDate = new Date(
-    document.getElementById("check-in-date") + "T00:00"
+    document.getElementById("check-in-date").value + "T00:00"
   );
   const numOfNights = document.getElementById("num-nights").value;
   const roomSelectElement = document.querySelector(
@@ -16,6 +20,13 @@ function getRoomRate() {
   const discountElement = document.querySelector(
     'input[name="discount-radio"]:checked'
   );
+  const numOfAdults = Number(document.getElementById("num-adults").value);
+  const numOfChildren = Number(document.getElementById("num-children").value);
+   
+    if (numOfAdults && numOfChildren > 4) {
+        document.getElementById("num-adults").innerText = "Can't exceed 4 adults";
+        document.getElementById("num-children").innerText = "Can't exceed 4 children";
+    }
 
   let priceOfRoom = 0;
 
@@ -31,7 +42,7 @@ function getRoomRate() {
     default:
       inSeason = false;
   }
-
+  //  function for the room price calculation
   if (
     (inSeason && roomSelectElement.value == "queen") ||
     roomSelectElement.value == "king"
@@ -49,4 +60,28 @@ function getRoomRate() {
   } else if (!inSeason && roomSelectElement.value == "2-bed-suite") {
     priceOfRoom = 210.0 * numOfNights;
   }
+
+  // discount pricing if statement
+  let priceOfDiscount = 0;
+  if (discountElement.value == "none") {
+    priceOfDiscount = 0;
+  }
+  if (discountElement.value == "aaa/senior") {
+    priceOfDiscount = 0.1 * priceOfRoom;
+  }
+  if (discountElement.value == "military") {
+    priceOfDiscount = 0.2 * priceOfRoom;
+  }
+
+   let taxes = 0.12 * (priceOfDiscount + priceOfRoom);
+  console.log(taxes);
 }
+
+totalRoomCost.innerHTML = priceOfRoom
+
+
+
+
+window.onload = () => {
+  getRoomRate();
+};
